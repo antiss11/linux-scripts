a80794abb2c7a7b964851e66daca47b8fcb51590
Dmitry Kulik <recc1337@gmail.com>
2022-08-01 16:08:00 +0300

    Code switched to work with pa

--- sound_devices.sh
+++ sound_devices.sh
@@ -1,29 +1,12 @@
 #!/bin/sh
-# Simple alsamixer controller is used to swith between speakers and headphones. Use --show option to show volume level, example for i3blocks.
-# Use --change option to mute one device and set volume to other device.
-# Use PCM channel to change volume.
 
-VOLUME_STATUS=$(amixer -c 1 get Headphone | awk 'FNR==7 {print $4}')            #check Headphones
-MASTER=$(amixer -c 1 get PCM | awk 'FNR==6 {print $5}'| grep -o [0-9]*)         #get PCM volume
+# Variables PA_PORT_1, PA_PORT_2 and PA_SINK_NAME can be received from "pacmd list" command
+# and added as environment variables for example in ~/.bashrc file.
 
-
-if [[ "$1" = "--show" ]]; then
-    if [ $VOLUME_STATUS = "0" ]; then
-        echo -e "\uF9C2 $MASTER"
-    else
-        echo -e "\uF7CA $MASTER"
-    fi
-
-elif [[ "$1" = "--change" ]]; then
-    if [ $VOLUME_STATUS = "0" ]; then                                                       #if Headphones is disabled - change to headphones
-        VOLUME_LEVEL=$(amixer -c 1 get Front | awk 'FNR==6 {print $5}' | grep -o [0-9]*)    #get speakers volume
-        amixer -c 1 set Front 0% > /dev/null
-        amixer -c 1 set Headphone $VOLUME_LEVEL
-    else
-        VOLUME_LEVEL=$(amixer -c 1 get Headphone | awk 'FNR==6 {print $5}' | grep -o [0-9]*)
-        amixer -c 1 set Headphone 0% > /dev/null
-        amixer -c 1 set Front $VOLUME_LEVEL
-    fi
+ACTIVE_PORTS=$(pacmd list | grep 'active port')
+if [[ $ACTIVE_PORTS == *"$PA_PORT_1"* ]];
+then
+    pacmd set-sink-port $PA_SINK_NAME $PA_PORT_2
+else
+    pacmd set-sink-port $PA_SINK_NAME $PA_PORT_1
 fi
-
-exit 0
